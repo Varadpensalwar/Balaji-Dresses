ed3c8058d6990e20958f03321f92dc8aeef779ef
Varad Pensalwar <varadpensalwar@gmail.com>
2025-04-12 23:47:20 +0530

    Update Image

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ import FeaturedProducts from "../components/FeaturedProducts";
 const categories = [
 	{ href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
 	{ href: "/t-shirts", name: "T-shirts", imageUrl: "/tshirts.jpg" },
-	{ href: "/shoes", name: "Shoes", imageUrl: "/shoes.jpg" },
+	{ href: "/saree", name: "saree", imageUrl: "/saree.jpg" },
 	{ href: "/glasses", name: "Glasses", imageUrl: "/glasses.png" },
 	{ href: "/jackets", name: "Jackets", imageUrl: "/jackets.jpg" },
 	{ href: "/suits", name: "Suits", imageUrl: "/suits.jpg" },
