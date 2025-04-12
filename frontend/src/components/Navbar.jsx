661f47ea05866c355494f4b4e57ecb1599cfd35f
Varad Pensalwar <varadpensalwar@gmail.com>
2025-04-12 21:59:29 +0530

    color update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,14 +12,14 @@ const Navbar = () => {
 		<header className='fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800'>
 			<div className='container mx-auto px-4 py-3'>
 				<div className='flex flex-wrap justify-between items-center'>
-					<Link to='/' className='text-2xl font-bold text-emerald-400 items-center space-x-2 flex'>
-						Balaji Dresses
+					<Link to='/' className='text-2xl font-bold text-amber-300 items-center space-x-2 flex'>
+						Balaji Dresses, Biloli
 					</Link>
 
 					<nav className='flex flex-wrap items-center gap-4'>
 						<Link
 							to={"/"}
-							className='text-gray-300 hover:text-emerald-400 transition duration-300
+							className='text-gray-300 hover:text-amber-300 transition duration-300
 					 ease-in-out'
 						>
 							Home
@@ -27,10 +27,10 @@ const Navbar = () => {
 						{user && (
 							<Link
 								to={"/cart"}
-								className='relative group text-gray-300 hover:text-emerald-400 transition duration-300 
+								className='relative group text-gray-300 hover:text-amber-300 transition duration-300 
 							ease-in-out'
 							>
-								<ShoppingCart className='inline-block mr-1 group-hover:text-emerald-400' size={20} />
+								<ShoppingCart className='inline-block mr-1 group-hover:text-amber-300' size={20} />
 								<span className='hidden sm:inline'>Cart</span>
 								{cart.length > 0 && (
 									<span
--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -25,7 +25,7 @@ const AdminPage = () => {
 		<div className='min-h-screen relative overflow-hidden'>
 			<div className='relative z-10 container mx-auto px-4 py-16'>
 				<motion.h1
-					className='text-4xl font-bold mb-8 text-emerald-400 text-center'
+					className='text-4xl font-bold mb-8 text-amber-300 text-center'
 					initial={{ opacity: 0, y: -20 }}
 					animate={{ opacity: 1, y: 0 }}
 					transition={{ duration: 0.8 }}
